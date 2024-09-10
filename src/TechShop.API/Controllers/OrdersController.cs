15f73402a41ce2e356444ec0ead1a7f6ffa7f298
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2024-09-09 23:19:18 -0300

    feat: enpoint post

--- src/TechShop.API/Controllers/OrdersController.cs
+++ src/TechShop.API/Controllers/OrdersController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TechShop.Application.Commands;
 using TechShop.Application.Queries;
 
 namespace TechShop.API.Controllers
@@ -12,12 +13,19 @@ namespace TechShop.API.Controllers
         private readonly IMediator _mediator = mediator;
 
         [HttpGet("{id:guid}")]
-        public async Task<ActionResult> GetByIdAsync(Guid id)
+        public async Task<ActionResult> GetOrderByIdAsync(Guid id)
         {
             var result = await _mediator.Send(new GetOrderById(id));
             if(result is null) return NotFound();
-
             return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateOrderAsync(CreateOrder command)
+        {
+            var orderId = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetOrderByIdAsync), orderId, command);
         }
     }
 }
