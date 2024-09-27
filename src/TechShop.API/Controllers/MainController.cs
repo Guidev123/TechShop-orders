3ade5222ef96cd6c8e3ec25483b37a6b7048c96e
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2024-09-27 16:43:42 -0300

    feat

---
+++ src/TechShop.API/Controllers/MainController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+using TechShop.Domain.Notifications;
+using TechShop.Infrasctructure.Notifications;
+
+namespace TechShop.API.Controllers
+{
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        private readonly INotificator _notificador;
+        protected MainController(INotificator notificador) => _notificador = notificador;
+        protected ActionResult CustomResponse(object result = null!)
+        {
+            if (ValidOperation())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notificador.GetNotifications().Select(n => n.Message)
+            });
+        }
+        protected bool ValidOperation() => !_notificador.HasNotification();
+        protected void NotifyError(string mensagem) => _notificador.Notify(new Notification(mensagem));
+    }
+}
---
+++ src/TechShop.Infrasctructure/Notifications/Notificator.cs
@@ -0,0 +1,13 @@
+﻿using TechShop.Domain.Notifications;
+
+namespace TechShop.Infrasctructure.Notifications
+{
+    public class Notificator : INotificator
+    {
+        private List<Notification> _notifications;
+        public Notificator() => _notifications = new List<Notification>();
+        public List<Notification> GetNotifications() => _notifications;
+        public bool HasNotification() => _notifications.Any();
+        public void Notify(Notification notification) => _notifications.Add(notification);  
+    }
+}
